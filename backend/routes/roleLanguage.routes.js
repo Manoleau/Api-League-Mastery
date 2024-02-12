0171c86b49e26732176726cf73e431a3436f3ded
Emmanuel Ardoin <emmanuelardoin@gmail.com>
2024-02-12 01:46:09 +0100

    update language of role

--- backend/routes/roleLanguage.routes.js
+++ backend/routes/roleLanguage.routes.js
@@ -3,8 +3,16 @@ const { addRoleLanguage, updateRoleLanguage } = require('../controllers/roleLang
 // require('../controllers/roleLanguage.controller')
 const router = express.Router();
 
+// GET
+// router.get("/",getRolesLanguages)
+// router.get("/roles_languages/:role_id/:language_id")
+// router.get("/languages/:language_id")
+// router.get("/roles/:role_id")
+
+
+// POST
 router.post("/", addRoleLanguage)
 
-router.put("/:role_id/:language_id", updateRoleLanguage)
+router.put("/role_id/:role_id/language_id/:language_id", updateRoleLanguage)
 
 module.exports = router;
